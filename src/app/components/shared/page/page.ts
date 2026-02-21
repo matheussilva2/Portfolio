d6f5d5793faa995ad139139f90c33f75cae08d39
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-21 12:53:48 -0300

    chore: basic project

---
+++ src/app/components/shared/page/page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../../../node_modules/@angular/router/types/_router_module-chunk";
+
+@Component({
+  selector: 'app-page',
+  imports: [RouterOutlet],
+  templateUrl: './page.html',
+  styleUrl: './page.css',
+})
+export class Page {
+
+}
