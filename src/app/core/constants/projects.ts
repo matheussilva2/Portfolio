a95040e2b2961f62be8c490c266f8c999a182191
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-22 17:09:43 -0300

    feat: added my projects constant and type

---
+++ src/app/core/constants/projects.ts
@@ -0,0 +1,15 @@
+export interface Project {
+    title: string;
+    image_url: string;
+    description: string;
+    tags: string[];
+}
+
+export const PROJECTS: Project[] = [
+    {
+        title: "AAAa",
+        image_url: "/images/languages.webp",
+        description: "BBBBBBB",
+        tags: ["AAAA", "BBBB", "CCCC", "DDDDD"]
+    }
+];
