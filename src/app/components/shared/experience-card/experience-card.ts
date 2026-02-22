642e74e0d18175be8c09000a7145b496b6cf56d6
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-22 18:24:35 -0300

    feat: experience card component

---
+++ src/app/components/shared/experience-card/experience-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-card',
+  imports: [],
+  templateUrl: './experience-card.html'
+})
+export class ExperienceCard {
+  readonly title = input.required<string>();
+  readonly description = input.required<string>();
+  readonly location = input.required<string>();
+  readonly schedule = input.required<string>();
+}
