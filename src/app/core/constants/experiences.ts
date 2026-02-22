8d1fc5a7249c66294c21c71986ff1795ec9ef1ce
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-22 18:24:44 -0300

    feat: experiences constant

---
+++ src/app/core/constants/experiences.ts
@@ -0,0 +1,33 @@
+export interface Experience {
+    title: string;
+    description: string;
+    location: string;
+    schedule: string;
+}
+
+export const EXPERIENCES: Experience[] = [
+    {
+        title: "Desenvolvedor Fullstack",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt omnis perferendis est dolorum sapiente. Inventore, consequatur minima nihil ex distinctio incidunt at, quae nesciunt iusto quam fuga, in aperiam!",
+        location: "Remoto",
+        schedule: "Fulltime"
+    },
+    {
+        title: "Desenvolvedor Fullstack",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt omnis perferendis est dolorum sapiente. Inventore, consequatur minima nihil ex distinctio incidunt at, quae nesciunt iusto quam fuga, in aperiam!",
+        location: "Remoto",
+        schedule: "Fulltime"
+    },
+    {
+        title: "Desenvolvedor Fullstack",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt omnis perferendis est dolorum sapiente. Inventore, consequatur minima nihil ex distinctio incidunt at, quae nesciunt iusto quam fuga, in aperiam!",
+        location: "Remoto",
+        schedule: "Fulltime"
+    },
+    {
+        title: "Desenvolvedor Fullstack",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt omnis perferendis est dolorum sapiente. Inventore, consequatur minima nihil ex distinctio incidunt at, quae nesciunt iusto quam fuga, in aperiam!",
+        location: "Remoto",
+        schedule: "Fulltime"
+    },
+];
