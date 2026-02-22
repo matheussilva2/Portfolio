8f1f22237e4d22d326ed09182887a5dc92d1f172
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-22 16:51:29 -0300

    feat: section title component

---
+++ src/app/components/shared/section-title/section-title.ts
@@ -0,0 +1,9 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-section-title',
+  imports: [],
+  templateUrl: './section-title.html',
+})
+export class SectionTitle {
+}
