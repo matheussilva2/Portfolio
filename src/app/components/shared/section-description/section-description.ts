41787c9ba7d2f359b0e89b03b56e6567c939f22f
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-22 16:52:07 -0300

    feat: section description text

---
+++ src/app/components/shared/section-description/section-description.ts
@@ -0,0 +1,9 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-section-description',
+  imports: [],
+  templateUrl: './section-description.html',
+})
+export class SectionDescription {
+}
