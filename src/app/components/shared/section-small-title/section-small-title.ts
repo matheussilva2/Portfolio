f556aa6e7bb30a335db4f75aa7e0feef5c9b1830
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-22 16:51:40 -0300

    feat: section small title component

---
+++ src/app/components/shared/section-small-title/section-small-title.ts
@@ -0,0 +1,9 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-section-small-title',
+  imports: [],
+  templateUrl: './section-small-title.html',
+})
+export class SectionSmallTitle {
+}
