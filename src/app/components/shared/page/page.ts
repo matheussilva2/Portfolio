211387d94d512ec75e361ad96335b25b21a0f0ab
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-22 17:11:00 -0300

    feat: page layout

--- src/app/components/shared/page/page.ts
+++ src/app/components/shared/page/page.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { Header } from "../header/header";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-page',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet],
   templateUrl: './page.html',
   styleUrl: './page.css',
 })
