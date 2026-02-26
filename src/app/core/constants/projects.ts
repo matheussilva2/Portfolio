f732fbc4917a2152ee94ca91b222614ad8d2e38e
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-26 19:11:36 -0300

    feat: added main projects

--- src/app/core/constants/projects.ts
+++ src/app/core/constants/projects.ts
@@ -1,51 +1,40 @@
 export interface Project {
     title: string;
-    image_url: string;
+    image_url: string[];
     description: string;
     tags: string[];
 }
 
 export const PROJECTS: Project[] = [
     {
-        title: "AAAa",
-        image_url: "/images/languages.webp",
-        description: "BBBBBBB",
-        tags: ["AAAA", "BBBB", "CCCC", "DDDDD"]
+        title: "Everconte",
+        image_url: [
+            "/images/projects/everconte/everconte-1.png",
+            "/images/projects/everconte/everconte-2.png",
+            "/images/projects/everconte/everconte-3.png",
+            "/images/projects/everconte/everconte-4.png",
+            "/images/projects/everconte/everconte-5.png",
+        ],
+        description: "Plataforma para automação de publicações em redes sociais em geral.",
+        tags: ["NextJs", "ReactJs", "HeroUI", "TailwindCSS", "Google Cloud"]
     },
     {
-        title: "AAAa",
-        image_url: "/images/languages.webp",
-        description: "BBBBBBB",
-        tags: ["AAAA", "BBBB", "CCCC", "DDDDD"]
+        title: "Dowcenter",
+        image_url: [
+            "/images/projects/dowcenter/dowcenter-1.png",
+            "/images/projects/dowcenter/dowcenter-2.png",
+            "/images/projects/dowcenter/dowcenter-3.png",
+            "/images/projects/dowcenter/dowcenter-4.png"
+        ],
+        description: "Landing page para marketing place de produtos de design.",
+        tags: ["NextJs", "ReactJs", "HeroUI", "TailwindCSS"]
     },
     {
-        title: "AAAa",
-        image_url: "/images/languages.webp",
-        description: "BBBBBBB",
-        tags: ["AAAA", "BBBB", "CCCC", "DDDDD"]
-    },
-    {
-        title: "AAAa",
-        image_url: "/images/languages.webp",
-        description: "BBBBBBB",
-        tags: ["AAAA", "BBBB", "CCCC", "DDDDD"]
-    },
-    {
-        title: "AAAa",
-        image_url: "/images/languages.webp",
-        description: "BBBBBBB",
-        tags: ["AAAA", "BBBB", "CCCC", "DDDDD"]
-    },
-    {
-        title: "AAAa",
-        image_url: "/images/languages.webp",
-        description: "BBBBBBB",
-        tags: ["AAAA", "BBBB", "CCCC", "DDDDD"]
-    },
-    {
-        title: "AAAa",
-        image_url: "/images/languages.webp",
-        description: "BBBBBBB",
-        tags: ["AAAA", "BBBB", "CCCC", "DDDDD"]
+        title: "UDOIS",
+        image_url: [
+            "/images/projects/udois/udois-1.png",
+        ],
+        description: "Marketing place de artes (convite de festa, topo de bolo, cartão de visita...) com artes personalizáveis através de um editor online.",
+        tags: ["Laravel", "JQuery", "Bootstrap", "Google Cloud Platform", "MySQL", "Docker"]
     },
 ];
